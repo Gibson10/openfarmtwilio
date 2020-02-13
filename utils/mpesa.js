a58e0ca3eee608089e58971ca2ff7da00ed34dcb
Gibson Munene <njine10@gmail.com>
2020-02-13 16:10:15 +0300

    mpesa hook

--- utils/mpesa.js
+++ utils/mpesa.js
@@ -25,7 +25,7 @@ function MpesaTransaction(phone){
     PartyA: phone,
     PartyB: 174379,
     PhoneNumber: phone,
-    CallBackURL: "https://138e197f.ngrok.io/mpesapayment",
+    CallBackURL: "https://openfarmwhatsapp.herokuapp.com/mpesapayment",
     AccountReference: "Payment",
     passKey: "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     TransactionType: "CustomerPayBillOnline" /* OPTIONAL */,
